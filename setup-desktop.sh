4619173b375c48cc50f51d1a5966dfd22a91c035
faxgeraet <31393177+faxgeraet@users.noreply.github.com>
2020-05-08 22:06:52 +0200

    Update setup-desktop.sh

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -1,9 +1,12 @@
 gsettings set org.gnome.desktop.interface gtk-theme 'Materia-compact'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+gsettings set org.gnome.settings-daemon.plugins.xsettings hinting 'slight'
+gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'rgba'
+gsettings set org.gnome.settings-daemon.plugins.xsettings rgba-order 'rgb'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto Mono 9.2998046875'
 gsettings set org.gnome.desktop.interface font-name 'Open Sans 9.3974609375'
 gsettings set org.gnome.desktop.interface document-font-name 'Open Sans 9.3984375'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action 'blank'
-gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'blank
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'blank'
